2af432350ec92dff427a6401b274e8fc15dfe515
faoroj <jfaoro15@gmail.com>
2025-03-09 21:47:04 -0400

    Signatureblends responsiveness

--- src/sections/signatureblends.jsx
+++ src/sections/signatureblends.jsx
@@ -6,18 +6,18 @@ const signatureblends = () => {
     <section className='max-container'>
 
       <div className="flex justify-center items-center flex-col">
-        <h1 className="text-[47px] font-lexend text-center text-primary">Discover Our Signature Blends</h1>
-        <p className="text-[16px] font-figtree max-w-[750px] mt-6 text-center text-lighterBrown">Indulge in our carefully curated selection of specialty coffee blends, 
+        <h1 className="sm:text-[47px] text-[36px] font-lexend text-center text-primary">Discover Our Signature Blends</h1>
+        <p className="sm:text-[16px] text-[14px] font-figtree max-w-[750px] mt-6 text-center text-lighterBrown">Indulge in our carefully curated selection of specialty coffee blends, 
           expertly roasted to perfection. Savor the rich flavors and delicate nuances of each cup, 
           crafted with meticulous attention to detail
         </p>
       </div>
 
-      <div className="flex flex-row justify-between gap-7 mt-[100px]">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 ss:grid-cols-1 grid-cols-1 gap-7 sm:mt-[70px] mt-[50px] place-items-center">
       {SignatureBlends.map((blend, i) => (
 
         <div key={i}>
-          <img src={blend.image} alt={blend.text} />
+          <img src={blend.image} alt={blend.text} className='h-auto w-full sm:max-w-[398px] max-h-[421px] object-contain' />
           <p className="mt-4 font-figtree text-[16px] text-lighterBrown">{blend.text}</p>
         </div>
 
